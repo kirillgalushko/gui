a7926db692855780017447f548267d62f4d25be8
Kirill Galushko <yoursgk@mail.ru>
2024-10-21 17:59:37 +0600

    Update release script

--- release.js
+++ release.js
@@ -22,35 +22,49 @@ const currentVersion = packageJson.version;
 
 console.log(`Current version: ${currentVersion}`);
 
-inquirer.prompt([
-  {
-    type: 'list',
-    name: 'releaseType',
-    message: 'Select the type of version bump:',
-    choices: ['patch', 'minor', 'major'],
-  }
-]).then((answers) => {
-  const newVersion = incrementVersion(currentVersion, answers.releaseType);
+let newVersion = currentVersion
+try {
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'releaseType',
+      message: 'Select the type of version bump:',
+      choices: ['patch', 'minor', 'major'],
+    }])
+    newVersion = incrementVersion(currentVersion, answers.releaseType);
 
-  packageJson.version = newVersion;
-  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+    packageJson.version = newVersion;
+    fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
 
-  console.log('Running build...');
-  execSync('npm run build', { stdio: 'inherit' });
+    console.log('Running build...');
+    execSync('npm run build', { stdio: 'inherit' });
 
-  console.log('Committing changes...');
-  execSync('git add package.json', { stdio: 'inherit' });
-  execSync('git add .', { stdio: 'inherit' });
-  execSync(`git commit -m "Release ${newVersion}"`, { stdio: 'inherit' });
+    console.log('Committing changes...');
+    execSync('git add package.json', { stdio: 'inherit' });
+    execSync('git add .', { stdio: 'inherit' });
+    execSync(`git commit -m "Release ${newVersion}"`, { stdio: 'inherit' });
 
-  console.log('Creating tag...');
-  execSync(`git tag -a v${newVersion} -m "Release ${newVersion}"`, { stdio: 'inherit' });
+    console.log('Creating tag...');
+    execSync(`git tag -a v${newVersion} -m "Release ${newVersion}"`, { stdio: 'inherit' });
 
-  console.log('Pushing changes and tags to remote...');
-  execSync('git push origin main', { stdio: 'inherit' });
-  execSync(`git push origin v${newVersion}`, { stdio: 'inherit' });
-
-  console.log(`New version ${newVersion} committed and tagged successfully.`);
-}).catch((error) => {
+    console.log(`New version ${newVersion} committed and tagged successfully.`);
+} catch(error) {
   console.error('An error occurred:', error);
-});
+}
+
+try {
+  const shouldPushAnswers = await inquirer.prompt([{
+    type: 'list',
+    name: 'shouldPush',
+    message: 'Do you want to push changes to git?',
+    choices: ['No', 'Yes'],
+  }])
+  if (shouldPushAnswers.shouldPush === 'Yes') {
+    console.log('Pushing changes and tags to remote...');
+    execSync('git push origin main', { stdio: 'inherit' });
+    execSync(`git push origin v${newVersion}`, { stdio: 'inherit' });
+  }
+  console.log(`New version ${newVersion} committed and tagged successfully.`);
+} catch(error) {
+  console.error('An error occurred:', error);
+}
