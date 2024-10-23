3b99b18f30c9490c6df3f24037c60b9b35d28321
Kirill Galushko <yoursgk@mail.ru>
2024-10-24 00:18:05 +0600

    Add tooltip component

---
+++ src/components/Tooltip/Tooltip.stories.ts
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from '@storybook/vue3';
+import Tooltip from './Tooltip.vue';
+import Text from '../Text/Text.vue'
+
+const meta: Meta<typeof Tooltip> = {
+  title: 'Example/Tooltip',
+  component: Tooltip,
+  tags: ['autodocs'],
+  argTypes: {
+    mode: { control: 'select', options: ['default', 'accent'] },
+  },
+  args: {
+    mode: 'default',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Tooltip>;
+
+export const Default: Story = {
+  args: {
+    mode: 'default',
+  },
+  render: (args) => ({
+    components: { Tooltip, Text },
+    setup() {
+      return { args };
+    },
+    template: `
+      Наведи курсор вот 
+      <Tooltip v-bind="args">
+        <template #popper>
+          Это подсказка
+        </template>
+        <strong>сюда</strong></Tooltip>, чтобы увидеть Tooltip.
+    `,
+  }),
+};
---
+++ src/components/Tooltip/Tooltip.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import 'floating-vue/dist/style.css';
+import { Tooltip } from 'floating-vue';
+
+export interface TooltipProps {
+  mode?: 'default' | 'accent'
+}
+
+const props = withDefaults(defineProps<TooltipProps>(), { mode: 'default' })
+</script>
+
+
+<template>
+  <Tooltip v-bind="$attrs" :arrowOverflow="false"
+    :popperClass="props.mode === 'accent' ? 'tooltip-accent' : 'tooltip-default'" :class="['tooltip', props.mode]">
+    <template #default>
+      <slot></slot>
+    </template>
+
+    <template #popper>
+      <slot name="popper"></slot>
+    </template>
+  </Tooltip>
+</template>
+
+<style scoped>
+.tooltip {
+  display: inline-block;
+}
+</style>
+<style>
+.tooltip-default {
+  --tooltip-text-color: hsl(var(--popover-foreground));
+  --tooltip-background-color: hsl(var(--popover));
+  --tooltip-border-color: hsl(var(--border));
+}
+
+.tooltip-accent {
+  --tooltip-text-color: hsl(var(--primary-foreground));
+  --tooltip-background-color: hsl(var(--primary));
+}
+
+.v-popper--theme-tooltip .v-popper__inner {
+  font-size: 14px;
+  border-radius: 6px;
+  padding: 6px 12px;
+  color: var(--tooltip-text-color);
+  background-color: var(--tooltip-background-color);
+  border: 1px solid var(--tooltip-border-color);
+}
+
+.v-popper__arrow-container {
+  display: none;
+}
+</style>
