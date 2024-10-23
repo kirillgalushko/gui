74f2803ac94854a2dcad54b01c770b377b565ec3
Kirill Galushko <yoursgk@mail.ru>
2024-10-23 14:50:56 +0600

    Release 2.0.0

--- dist/types/components/Icon/Icon.vue.d.ts
+++ dist/types/components/Icon/Icon.vue.d.ts
@@ -2,6 +2,9 @@ export interface IconProps {
     name: string;
     className?: string;
     style?: string;
+    variant?: 'filled' | 'outline';
 }
-declare const _default: import("vue").DefineComponent<IconProps, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<IconProps> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
+declare const _default: import("vue").DefineComponent<IconProps, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<IconProps> & Readonly<{}>, {
+    variant: "filled" | "outline";
+}, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
 export default _default;
