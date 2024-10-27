ae5af80147a2e4da08adcc1f0a1ccb2efca2d5c8
Kirill Galushko <yoursgk@mail.ru>
2024-10-27 15:45:23 +0600

    Add badge component

---
+++ src/components/Badge/Badge.stories.ts
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from '@storybook/vue3';
+import Badge from './Badge.vue';
+
+const meta: Meta<typeof Badge> = {
+  title: 'Example/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  argTypes: {
+    mode: {
+      control: { type: 'select' },
+      options: ['default', 'secondary']
+    },
+  },
+  args: {
+    mode: 'default'
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Badge>;
+
+export const Default: Story = {
+  args: {},
+  render: (args) => ({
+    components: { Badge },
+    setup() {
+      return { args };
+    },
+    template: `<Badge v-bind="args">99+</Badge>`,
+  }),
+};
+
+export const Secondary: Story = {
+  args: {
+    mode: 'secondary',
+  },
+  render: (args) => ({
+    components: { Badge },
+    setup() {
+      return { args };
+    },
+    template: `<Badge v-bind="args">99+</Badge>`,
+  }),
+};
---
+++ src/components/Badge/Badge.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+interface BadgeProps {
+  mode: 'default' | 'secondary' | 'ghost'
+}
+
+const props = withDefaults(defineProps<BadgeProps>(), { mode: 'default' })
+
+</script>
+
+<template>
+  <div :class="['Badge', props.mode]">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.Badge {
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 12px;
+  padding: 2px 6px;
+  border-radius: 999px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: auto;
+}
+
+.default {
+  background-color: hsl(var(--primary));
+  color: hsl(var(--primary-foreground));
+}
+
+.secondary {
+  background-color: hsl(var(--secondary));
+  color: hsl(var(--secondary-foreground));
+}
+
+.ghost {
+  background-color: transparent;
+}
+</style>
