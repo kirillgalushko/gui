7b498be7cc28020f355bc0adb74e13d237285c47
Kirill Galushko <yoursgk@mail.ru>
2024-11-02 21:02:54 +0600

    Fix context menu visibility

--- src/components/ContextMenu/useContextMenu.ts
+++ src/components/ContextMenu/useContextMenu.ts
@@ -1,6 +1,14 @@
 import { onBeforeUnmount, onMounted, ref } from "vue";
 import { useFloating, flip, shift } from '@floating-ui/vue';
 
+const isEventTargetWithinRef = (target: Node | null, refNode: HTMLElement | null): boolean => {
+  while (target) {
+    if (target === refNode) return true;
+    target = (target as HTMLElement).parentNode;
+  }
+  return false;
+}
+
 export const useContextMenu = () => {
   const isContextMenuVisible = ref(false);
   const position = ref({ x: 0, y: 0 });
@@ -15,6 +23,7 @@ export const useContextMenu = () => {
   });
 
   const showContextMenu = (event: MouseEvent) => {
+    console.log('show')
     event.preventDefault();
     position.value = { x: event.clientX, y: event.clientY };
     isContextMenuVisible.value = true;
@@ -22,21 +31,29 @@ export const useContextMenu = () => {
   }
 
   const hideContextMenu = () => {
-    isContextMenuVisible.value = false;
+      isContextMenuVisible.value = false;
+  }
+
+  const handleDocumentContextMenu = (event: MouseEvent) => {
+    if (targetRef.value && !isEventTargetWithinRef(event.target as Node, targetRef.value)) {
+      isContextMenuVisible.value = false;
+    }
   }
 
   onMounted(() => {
     if (targetRef.value) {
       targetRef.value.addEventListener('contextmenu', showContextMenu);
       document.addEventListener('click', hideContextMenu);
+      document.addEventListener('contextmenu', handleDocumentContextMenu);
     }
   })
 
   onBeforeUnmount(() => {
     if (targetRef.value) {
       targetRef.value.removeEventListener('contextmenu', showContextMenu);
+      document.removeEventListener('click', hideContextMenu);
+      document.addEventListener('contextmenu', handleDocumentContextMenu);
     }
-    document.removeEventListener('click', hideContextMenu);
   })
 
   return { contextMenuData: { floatingStyles, floatingRef, position, positionRef, isContextMenuVisible }, targetRef }
