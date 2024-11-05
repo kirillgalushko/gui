5808c1bfdefd12b36d84d3b2b2a43cc028069f1c
kirillgalushko <kirillgalushko@users.noreply.github.com>
2024-11-05 12:07:38 +0000

    deploy: 9d5bdc259eeb659bd55447e224663ae9a62038d0

--- assets/BaseMenu.stories-CoEWj5fB.js
+++ assets/BaseMenu.stories-CoEWj5fB.js
@@ -1,4 +1,4 @@
-import{B as t}from"./BaseMenu-Bok6tjVV.js";import{B as m}from"./BaseMenuItem-Cu0YWnj8.js";import{B as o,a as u}from"./BaseMenuSeparator-VEv7U2wP.js";import{I as B}from"./Icon-DuNk9YsD.js";import"./vue.esm-bundler-0yPEwasB.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const d={title:"Example/BaseMenu",component:t,tags:["autodocs"]},e={args:{},render:r=>({components:{BaseMenu:t,BaseMenuItem:m,BaseMenuList:o,BaseMenuSeparator:u,Icon:B},setup(){return{args:r}},template:`
+import{B as t}from"./BaseMenu-Bok6tjVV.js";import{B as m}from"./BaseMenuItem-DRfFKY4T.js";import{B as o,a as u}from"./BaseMenuSeparator-VEv7U2wP.js";import{I as B}from"./Icon-DuNk9YsD.js";import"./vue.esm-bundler-0yPEwasB.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const d={title:"Example/BaseMenu",component:t,tags:["autodocs"]},e={args:{},render:r=>({components:{BaseMenu:t,BaseMenuItem:m,BaseMenuList:o,BaseMenuSeparator:u,Icon:B},setup(){return{args:r}},template:`
       <BaseMenu v-bind="args">
         <BaseMenuList>
           <BaseMenuItem><Icon name="pencil" />Открыть</BaseMenuItem>
