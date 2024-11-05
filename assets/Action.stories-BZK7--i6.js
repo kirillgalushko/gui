1a4fe706b29e2e5bcae92d0c69868f17adb777e8
kirillgalushko <kirillgalushko@users.noreply.github.com>
2024-11-05 12:43:36 +0000

    deploy: 62c272420f36b87e1508751f91b055b3530a6fed

---
+++ assets/Action.stories-BZK7--i6.js
@@ -0,0 +1,15 @@
+import{A as r}from"./Action-BXFPgI-T.js";import{I as s}from"./Icon-DuNk9YsD.js";import"./vue.esm-bundler-0yPEwasB.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const d={title:"Example/Action",component:r,tags:["autodocs"],argTypes:{}},n={args:{},render:a=>({components:{Action:r,Icon:s},setup(){return{args:a}},template:'<Action v-bind="args"> <Icon name="eye" /> </Action>'})};var e,t,o;n.parameters={...n.parameters,docs:{...(e=n.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {},
+  render: args => ({
+    components: {
+      Action,
+      Icon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<Action v-bind="args"> <Icon name="eye" /> </Action>\`
+  })
+}`,...(o=(t=n.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const u=["Default"];export{n as Default,u as __namedExportsOrder,d as default};
