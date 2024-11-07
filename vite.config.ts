bf50b29e393b19515cbdcfe72636006a141111d8
Kirill Galushko <yoursgk@mail.ru>
2024-11-07 22:33:37 +0600

    Add code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import libAssetsPlugin from "@laynezh/vite-plugin-lib-assets";
 
 export default defineConfig({
   plugins: [
@@ -12,12 +14,26 @@ export default defineConfig({
       ],
       symbolId: 'icon-[dir]-[name]',
     }),
+    libInjectCss(),
+    libAssetsPlugin({
+      include: /\.(eot|woff2?|ttf)(\?.*)?(#.*)?$/,
+      name: "fonts/[name].[ext]",
+    }),
+    libAssetsPlugin({
+      include: /\.(svg)(\?.*)?(#.*)?$/,
+      name: "svg/[name].[ext]",
+    }),
+    libAssetsPlugin({
+      include: /\.(png|jpeg|jpg|gif|webp)(\?.*)?(#.*)?$/,
+      name: "images/[name].[ext]",
+    }),
   ],
   build: {
     lib: {
       entry: 'src/index.ts',
       name: 'gui',
       fileName: (format) => `gui.${format}.js`,
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['vue'],
@@ -25,6 +41,19 @@ export default defineConfig({
         globals: {
           vue: 'Vue',
         },
+        chunkFileNames: 'chunks/[name]-[hash].js',
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+          if (id.includes('src/components/')) {
+            const componentName = id.split('src/components/')[1].split('/')[0];
+            return `components/${componentName}`;
+          }
+        },
+        inlineDynamicImports: false,
       },
     },
   },
