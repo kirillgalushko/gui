129bb92fe2b7a9713583d4b456ec0bed38f8eb38
kirillgalushko <kirillgalushko@users.noreply.github.com>
2026-06-18 12:40:49 +0000

    deploy: c0f592b20ac9b8c5c71614e9d62aa0395379935a

---
+++ assets/DocsRenderer-CFRXHY34-ypf7jpgy.js
@@ -0,0 +1,1 @@
+import{D as n,d as f}from"./chunk-NUUEMKO5-BYX9sjFx.js";import"./iframe-KOZevdoA.js";import"../sb-preview/runtime.js";import"./index-B0EJvtKl.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C3o3hejL.js";import"./index-D-8MO0q_.js";import"./index-CxISiSMm.js";import"./index-DrFu-skq.js";export{n as DocsRenderer,f as defaultComponents};
