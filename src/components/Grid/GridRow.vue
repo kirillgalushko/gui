518abf65ffa434c1688721be127661f66473032f
Kirill Galushko <yoursgk@mail.ru>
2026-06-30 13:33:41 +0300

    Add row gap property

--- src/components/Grid/GridRow.vue
+++ src/components/Grid/GridRow.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+export interface GridRowProps {
+  rowGap?: string;
+}
+
+const props = withDefaults(defineProps<GridRowProps>(), {
+  rowGap: '0',
+});
+</script>
+
 <template>
-  <div class="grid-row">
+  <div class="grid-row" :style="{ '--grid-row-gap': props.rowGap }">
     <slot></slot>
   </div>
 </template>
@@ -9,7 +19,7 @@
   display: grid;
   grid-template-columns: repeat(var(--grid-columns, 12), minmax(0, 1fr));
   column-gap: var(--grid-gutter, var(--gap-4, 16px));
-  row-gap: 0;
+  row-gap: var(--grid-row-gap, 0);
   width: 100%;
   min-width: 0;
 }
